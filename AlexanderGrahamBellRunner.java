54e0863133040fbddb1d996b13ed4d24d917b045
Albert Tieu <albert.tieu@mrjaffesclass.com>
2023-05-02 13:54:25 -0700

    initial commit

---
+++ AlexanderGrahamBellRunner.java
@@ -0,0 +1,31 @@
+ 
+
+import java.util.Scanner;
+
+/**
+ * A simple class to run the Magpie class.
+ * @author Laurie White
+ * @version April 2012
+ */
+public class AlexanderGrahamBellRunner
+{
+
+    /**
+     * Create a Magpie, give it user input, and print its replies.
+     */
+    public static void main(String[] args)
+    {
+        AlexanderGrahamBell alexBell = new AlexanderGrahamBell();
+        
+        System.out.println (alexBell.getGreeting());
+        Scanner in = new Scanner (System.in);
+        String statement = in.nextLine();
+        
+        while (!statement.equals("Bye"))
+        {
+            System.out.println (alexBell.getResponse(statement));
+            statement = in.nextLine();
+        }
+    }
+
+}
